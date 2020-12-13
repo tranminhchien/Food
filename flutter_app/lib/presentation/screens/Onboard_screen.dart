a323cc5608178a27bb7f0449eb37a6ae95f9b33f
MinhChien <thanhutdx22@gmail.com>
2020-12-13 12:56:57 +0700

    first

---
+++ flutter_app/lib/presentation/screens/Onboard_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OnboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+              children:<Widget>[
+                  ClipRRect(
+                    child: Container(
+                    height: 73,
+                    width: 73,
+                    color: Colors.red,
+                    child: Image.asset("assets/images/Icon_Onboard"),
+                    ),
+                  ),
+              ],
+        ),
+      ),
+    );
+  }
+}
